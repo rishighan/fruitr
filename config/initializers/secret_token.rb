454d16f9cadff843c138fa13dab65e090df6fdfd
Rishi Ghan <rishi.ghan@gmail.com>
2012-05-09 17:08:40 -0400

    adding all files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fruitr::Application.config.secret_token = '055b00f308781ab9f9b6c1bf4a631e9fc5e53feb15db6ec7142df3c5f67d5377110aa8a1992a65f8f6edcf3cbd466adbe23c153c5fbef1d9f642263d78f12aa4'
