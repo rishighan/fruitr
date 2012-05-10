bf0009574dc7fbbb7ae4a7bd34b1a1f5f50f725d
Rishi Ghan <rishi.ghan@gmail.com>
2012-05-10 18:14:17 -0400

    adding support for JSON parsing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
+  require 'yajl/http_stream'
   def index
     redirect_to "http://www.last.fm/api/auth/?api_key=#{API_KEY}"
     
   end
   
   def fruitify
-    @similarArtists = ActiveSupport::JSON.decode("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=#{params[:name]}&format=json&api_key=#{API_KEY}")
-  
+    parser =Yajl::Parser.new
+    url = URI.parse("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=#{params[:name]}&format=json&api_key=#{API_KEY}")
+    @result = Yajl::HttpStream.get(url)
+    
     render :layout => 'application'
  end
 
