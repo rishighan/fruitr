1f77413712dae4801d8b394a8fea4f60d8fcf3de
Rishi Ghan <rishi.ghan@gmail.com>
2012-05-11 07:17:02 -0400

    last night's changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,24 @@
 class PagesController < ApplicationController
   require 'yajl/http_stream'
+  before_filter :set_artist
   def index
     redirect_to "http://www.last.fm/api/auth/?api_key=#{API_KEY}"
     
   end
   
   def fruitify
-    parser =Yajl::Parser.new
-    url = URI.parse("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=#{params[:name]}&format=json&api_key=#{API_KEY}")
-    @result = Yajl::HttpStream.get(url) 
     
+    begin
+      url = URI.parse("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=#{params[:name]}&format=json&api_key=#{API_KEY}")
+      @result = Yajl::HttpStream.get(url)
+    rescue NoMethodError
+      
+    end  
     render :layout => 'application'
  end
 
+ def set_artist
+   
+  
+ end
 end
