d86ddb440ac048717768a51d19ea9f3477c3274f
Rishi Ghan <rishi.ghan@gmail.com>
2012-05-31 17:35:09 -0400

    changes to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "fruitr"
+set :repository,  "git@github.com:rishighan/fruitr.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+server "rishighan.com", :app, :web, :db, :primary => true 
+
+
+set :deploy_to, "/home/rishigha"
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
