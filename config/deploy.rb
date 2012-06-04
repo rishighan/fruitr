86ae44e2cf38ca4963a291390d26d42bb4067eba
Rishi Ghan <rishi.ghan@gmail.com>
2012-06-04 16:13:01 -0400

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,9 @@ set :application, "fruitr"
 set :keep_releases, 1
 
 set :scm, :git
-set :scm_username, "root"
+set :scm_username, "rishighan"
 set :scm_password, Proc.new {CLI.password_prompt "Password: "}
-set :repository, "git@github.com:rishighan/fruitr.git"
+set :repository, "git@github.com:rishighan/#{application}.git"
 set :branch, "master"
 set :rails_env, "production"
 set :deploy_via, :remote_cache
@@ -18,21 +18,6 @@ set :user, "rishigha"
 set :server_name, "rishighan.com"
 server "rishighan.com", :app, :web, :db, :primary => true 
 
-# ==============================
-# Bundler
-# ==============================
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(current_release, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-  end
- 
-  task :bundle_new_release, :roles => :app do
-    bundler.create_symlink
-    run "cd #{release_path} && bundle install --without test"
-  end
-end
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -42,6 +27,7 @@ set :deploy_to, "/home/#{user}/#{application}"
 set :use_sudo, false
 #set :group_writable, false
 default_run_options[:pty] = true 
+ssh_options[:forward_agent] = true
 
 role :app, server_name
 role :web, server_name
