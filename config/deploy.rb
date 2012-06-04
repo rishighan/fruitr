d970829ffe14a5de43d9ba1ef85e7c12dedcd3ce
Rishi Ghan <frishi@me.com>
2012-06-04 10:20:12 -0400

    capistrano recipes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,35 @@
 set :application, "fruitr"
-set :repository,  "git@github.com:rishighan/fruitr.git"
+
+default_run_options[:pty] = true 
 
 set :scm, :git
+set :scm_username, "rishighan"
+set :repository, "git@github.com:rishighan/fruitr.git"
+set :branch, "master"
+set :rails_env, "production"
+set :deploy_to, "/home/rishigha/fruitr"
+set :deploy_via, :remote_cache
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+set :user, "root"
+set :domain, "rishighan.com"
 server "rishighan.com", :app, :web, :db, :primary => true 
 
-
-set :deploy_to, "/home/rishigha"
-
-# if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
+
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
+ 
+ 
+ 
